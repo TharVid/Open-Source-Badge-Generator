73c8b95490104223d85a0ebe8e8a068ef0f419a9
Helen Xu <helen.z.xu@duke.edu>
2020-10-18 23:28:09 -0400

    fixed formatting and added comments in app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //https://api.github.com/users/user_name/repos
-//
 
 const user_img = document.querySelector(".user_img");
 const userName = document.querySelector(".user_name h1");
@@ -8,26 +7,26 @@ const repo_details = document.querySelector(".repo_details");
 const btn_submit = document.querySelector(".btn_submit");
 const followe = document.querySelector(".followe span");
 
-
-
 let user_name = '';
 
-//when user writer user name in text box
+//when user enters username in text box
 function inputFunction() {
+    //trim removes leading and trailing white space from user input
     let input_user = document.querySelector(".input_user").value.trim();
-    //trim method will replace before and after white space of given calue
 
+    //if invalid username
     if (input_user.length <= 0) {
         alert("Please enter github user name");
         document.querySelector(".input_user").value = "";
         document.querySelector(".input_user").focus();
         return false;
-    } else {
+    } 
+    //if username is valid, call fetchUser()
+    else {
         user_name = input_user.split("").join("");
-        //if everything is ok run fetch user funciton
-        fetchUser(); // this funciton is not made yet
+        fetchUser();
 
-        //clear the input box and focused it for next
+        //clear the input box and refocus it
         document.querySelector(".input_user").value = "";
         document.querySelector(".input_user").focus();
     }
@@ -37,7 +36,7 @@ btn_submit.addEventListener("click", function () {
     inputFunction()
 });
 
-// if user press enter it should be submit 
+//if user presses enter it should submit 
 document.querySelector(".input_user").addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
         //alert("you have pressed enter key");
@@ -45,24 +44,19 @@ document.querySelector(".input_user").addEventListener("keyup", function (e) {
     }
 })
 
-//fetching user from github api
+//fetch user info from github api
 function fetchUser() {
-    fetch(`https://api.github.com/users/${user_name}`)
-        .then(response => response.json())
-        .then(function (data) {
-           
-            console.log(data);
-            if (data.message === "Not Found") {
-                alert("user not found");
-                return false;
-            } else {
-                user_img.innerHTML = `<img src="${data.avatar_url}">`;
-                userName.innerHTML = data.login;
-                followers_.innerHTML = data.bio;
-                followe.innerHTML = data.name;
-
-            }
-        })
-
-  
+    fetch(`https://api.github.com/users/${user_name}`).then(response => response.json()).then(function (data) {
+        console.log(data);
+        if (data.message === "Not Found") {
+            alert("user not found");
+            return false;
+        } 
+        else {
+            user_img.innerHTML = `<img src="${data.avatar_url}">`;
+            userName.innerHTML = data.login;
+            followers_.innerHTML = data.bio;
+            followe.innerHTML = data.name;
+        }
+    })
 }
